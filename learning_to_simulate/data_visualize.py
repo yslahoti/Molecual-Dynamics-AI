c773b5db83d8a0780120998fa6dc78a4fa15717a
ishaan <ishaangp@gmail.com>
2020-12-25 13:26:00 -0500

    some shit
    Signed-off-by: ishaan <ishaangp@gmail.com>

--- learning_to_simulate/data_visualize.py
+++ learning_to_simulate/data_visualize.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 import tensorflow as tf
 import numpy as np
 import functools
@@ -5,13 +8,21 @@ from learning_to_simulate import reading_utils
 
 print(tf.__version__)
 
+
+def _read_metadata(data_path):
+    with open(os.path.join(data_path, 'metadata.json'), 'rt') as fp:
+        return json.loads(fp.read())
+
 tf.compat.v1.enable_eager_execution()
 
 datapath = '/private/tmp/datasets/WaterRamps/valid.tfrecord'
+datapath2 = '/private/tmp/datasets/WaterRamps/metadata.json'
 
-raw_dataset = tf.data.TFRecordDataset(datapath)
-ds = raw_dataset
+
+ds = tf.data.TFRecordDataset(datapath)
+metadata = _read_metadata(datapath2)
 
 ds = ds.map(functools.partial(
     reading_utils.parse_serialized_simulation_example, metadata=metadata))
 
+print(ds)
