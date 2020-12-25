b7004559ca07e629b194b46025a8a636f5b32210
yslahoti <yslahoti@seas.upenn.edu>
2020-12-25 13:00:58 -0500

    ?

--- learning_to_simulate/data_visualize.py
+++ learning_to_simulate/data_visualize.py
@@ -9,7 +9,11 @@ datapath = '/private/tmp/datasets/MultiMaterial/valid.tfrecord'
 
 raw_dataset = tf.data.TFRecordDataset(datapath)
 
+for example in tf.python_io.tf_record_iterator(datapath):
+    print(tf.train.Example.FromString(example))
+
 for raw_record in raw_dataset.take(1):
     example = tf.train.Example()
     example.ParseFromString(raw_record.numpy())
     print(example)
+
