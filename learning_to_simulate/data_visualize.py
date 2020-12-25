65a71a3efbef0151519c331eb30a6c8e882a43ea
yslahoti <yslahoti@seas.upenn.edu>
2020-12-25 12:44:54 -0500

    ?

--- learning_to_simulate/data_visualize.py
+++ learning_to_simulate/data_visualize.py
@@ -2,3 +2,7 @@ import tensorflow as tf
 import numpy as np
 
 print(tf.__version__)
+datapath = '/private/tmp/datasets/WaterRamps/valid.tfrecord'
+
+for example in tf.python_io.tf_record_iterator(datapath):
+    print(tf.train.Example.FromString(example))
