9cdf73857cf04e4887dc3fb379b67cb997d9be32
ishaan <ishaangp@gmail.com>
2020-12-25 12:57:47 -0500

    deleted test folder
    Signed-off-by: ishaan <ishaangp@gmail.com>

--- learning_to_simulate/data_visualize.py
+++ learning_to_simulate/data_visualize.py
@@ -2,7 +2,14 @@ import tensorflow as tf
 import numpy as np
 
 print(tf.__version__)
-datapath = '/private/tmp/datasets/WaterRamps/valid.tfrecord'
 
-for example in tf.python_io.tf_record_iterator(datapath):
-    print(tf.train.Example.FromString(example))
+tf.compat.v1.enable_eager_execution()
+
+datapath = '/private/tmp/datasets/MultiMaterial/valid.tfrecord'
+
+raw_dataset = tf.data.TFRecordDataset(datapath)
+
+for raw_record in raw_dataset.take(1):
+    example = tf.train.Example()
+    example.ParseFromString(raw_record.numpy())
+    print(example)
