7895648a402c59675890e6cee6718f42c9622f74
yslahoti <yslahoti@seas.upenn.edu>
2021-01-06 23:55:27 -0500

    ?

--- learning_to_simulate/tfrec_explore.py
+++ learning_to_simulate/tfrec_explore.py
@@ -17,21 +17,27 @@ from learning_to_simulate import noise_utils
 from learning_to_simulate import reading_utils
 import md
 
-def getData_tensor(t,p):
+def make_dict_tensor(t,p,num):
     t_tensor = tf.convert_to_tensor(t)
     p_tensor = tf.convert_to_tensor(p)
-    return t_tensor, p_tensor
 
-def make_dict(t,p,num):
     type_dict = {
-      "particle_type": t,
+      "particle_type": t_tensor,
       "key": tf.convert_to_tensor(num),
     }
     pos_dict = {
-      "position": p
+      "position": p_tensor
     }
     return ((type_dict, pos_dict))
 
 
-# for each trajectory, call getData_tensor to convert to tensor then make_dict
-# to get tuple dictionary. After running on all
+# making TF dataset
+num_trajectory = 3
+all_dat = [];
+for i in range(0,num_trajectory - 1)):
+#    t,p = function to get all the t and p for a trajectory
+    all_dat.append(make_dict_tensor(t,p,i))
+ds = tf.data.Dataset.from_tensor_slices(all_dat)
+
+
+
